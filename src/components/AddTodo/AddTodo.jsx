4d624aa1d8421351574322a543bf85e3e93de6cb
Kyung <kyung.mi.kang@gritacademy.se>
2023-12-14 21:15:02 +0100

    14-5

--- src/components/AddTodo/AddTodo.jsx
+++ src/components/AddTodo/AddTodo.jsx
@@ -1,26 +1,48 @@
-import React, { useState } from 'react';
+import React,{useState} from 'react';
 
 export default function AddTodo({onAdd}) {
-    const [text, setText] = useState('');
-    
-    const handleSubmit=(e)=>{
-        e.preventDefault();
-       
-        if(text.trim().length ===0){
-          return;
-        }
-        onAdd({id:'고유한값', text, status:'active' });
-        
-        setText('');
-    };
-   
-    const handleChange=(e)=>{setText(e.target.value);
-    };
+const [text, setText]= useState('');
+
+const handleChange =(e)=>setText(e.target.value);
+
+const handleSubmit =(e)=>{
+    e.preventDefault();
+    if( text.trim().length === 0 ) return;
+     onAdd({id:'unique', text, status:'active'});
+    setText('');
+}
 
 
     return <form onSubmit={handleSubmit}>
-        <input type='text' placeholder='Add Todo' value={text} onChange={handleChange} />
-        <button>ADD Todo</button>
-    </form>;
+        <input type='text' placeholder='add todolist' value={text} onChange={handleChange}/>
+        <button>ADD</button>
+    </form>
 }
+
+
+// import React, { useState } from 'react';
+
+// export default function AddTodo({onAdd}) {
+//     const [text, setText] = useState('');
+    
+//     const handleSubmit=(e)=>{
+//         e.preventDefault();
+       
+//         if(text.trim().length ===0){
+//           return;
+//         }
+//         onAdd({id:'고유한값', text, status:'active' });
+        
+//         setText('');
+//     };
+   
+//     const handleChange=(e)=>{setText(e.target.value);
+//     };
+
+
+//     return <form onSubmit={handleSubmit}>
+//         <input type='text' placeholder='Add Todo' value={text} onChange={handleChange} />
+//         <button>ADD Todo</button>
+//     </form>;
+// }
 
--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,29 +1,59 @@
 import React, {useState} from 'react';
-import AddTodo from '../AddTodo/AddTodo';
+import AddTodo from '../AddTodo/AddTodo'
 
 export default function TodoList() {
     const [todos, setTodos] =useState([
-        {id: '123',
-         text: 'Shopping',
-         status: 'active',
-        },
+                {id: '123',
+                 text: 'Shopping',
+                 status: 'active',
+                },
+        
+                {id: '124',
+                 text: 'Studying',
+                 status: 'active',
+                }   
+            ]);
 
-        {id: '124',
-         text: 'Studying',
-         status: 'active',
-        }   
-    ]);
-
-    //add new item
-    const handleAdd=(todo)=>{
-             setTodos([...todos, todo])
-    }
-
+    const handleAdd=(todo)=>setTodos([...todos, todo]);
+        
     return <section>
-           <ul>{
-                todos.map(item =><li key={item.id}>{item.text}</li>)            
-            }</ul> 
-            <AddTodo    onAdd={handleAdd}/>
-            </section>
+
+
+
+<ul>{todos.map(item =><li key={item.id}>{item.text}</li>)}</ul> 
+
+            <AddTodo onAdd={handleAdd} />
+        </section>
 }
+
+
+// import React, {useState} from 'react';
+// import AddTodo from '../AddTodo/AddTodo';
+
+
+// export default function TodoList() {
+//     const [todos, setTodos] =useState([
+//         {id: '123',
+//          text: 'Shopping',
+//          status: 'active',
+//         },
+
+//         {id: '124',
+//          text: 'Studying',
+//          status: 'active',
+//         }   
+//     ]);
+
+//     //add new item
+//     const handleAdd=(todo)=>{
+//              setTodos([...todos, todo])
+//     }
+
+//     return <section>
+//            <ul>{
+//                 todos.map(item =><li key={item.id}>{item.text}</li>)            
+//             }</ul> 
+//             <AddTodo    onAdd={handleAdd}/>
+//             </section>
+// }
 
