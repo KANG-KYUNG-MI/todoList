0b15783776ab2258764d16eeea2bce508c96918a
Kyung <kyung.mi.kang@gritacademy.se>
2023-12-15 11:02:56 +0100

    15-1

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import AddTodo from '../AddTodo/AddTodo'
+import AddTodo from '../AddTodo/AddTodo';
 
 export default function TodoList() {
     const [todos, setTodos] =useState([
@@ -25,35 +25,3 @@ export default function TodoList() {
             <AddTodo onAdd={handleAdd} />
         </section>
 }
-
-
-// import React, {useState} from 'react';
-// import AddTodo from '../AddTodo/AddTodo';
-
-
-// export default function TodoList() {
-//     const [todos, setTodos] =useState([
-//         {id: '123',
-//          text: 'Shopping',
-//          status: 'active',
-//         },
-
-//         {id: '124',
-//          text: 'Studying',
-//          status: 'active',
-//         }   
-//     ]);
-
-//     //add new item
-//     const handleAdd=(todo)=>{
-//              setTodos([...todos, todo])
-//     }
-
-//     return <section>
-//            <ul>{
-//                 todos.map(item =><li key={item.id}>{item.text}</li>)            
-//             }</ul> 
-//             <AddTodo    onAdd={handleAdd}/>
-//             </section>
-// }
-
