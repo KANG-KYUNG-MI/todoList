3b5736b45c882178f65144b672ea0176a3d0fafd
Kyung <kyung.mi.kang@gritacademy.se>
2023-12-15 13:56:05 +0100

    15-4

--- src/components/AddTodo/AddTodo.jsx
+++ src/components/AddTodo/AddTodo.jsx
@@ -15,35 +15,8 @@ const handleSubmit =(e)=>{
 
 
     return <form onSubmit={handleSubmit}>
+        
         <input type='text' placeholder='add todolist' value={text} onChange={handleChange}/>
         <button>ADD</button>
     </form>
 }
-
-
-// import React, { useState } from 'react';
-
-// export default function AddTodo({onAdd}) {
-//     const [text, setText] = useState('');
-    
-//     const handleSubmit=(e)=>{
-//         e.preventDefault();
-       
-//         if(text.trim().length ===0){
-//           return;
-//         }
-//         onAdd({id:'고유한값', text, status:'active' });
-        
-//         setText('');
-//     };
-   
-//     const handleChange=(e)=>{setText(e.target.value);
-//     };
-
-
-//     return <form onSubmit={handleSubmit}>
-//         <input type='text' placeholder='Add Todo' value={text} onChange={handleChange} />
-//         <button>ADD Todo</button>
-//     </form>;
-// }
-
--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,8 +1,9 @@
 import React, {useState} from 'react';
 import AddTodo from '../AddTodo/AddTodo';
+import Todo from '../Todo/Todo';
 
 export default function TodoList() {
-    const [todos, setTodos] =useState([
+            const [todos, setTodos] =useState([
                 {id: '123',
                  text: 'Shopping',
                  status: 'active',
@@ -14,14 +15,29 @@ export default function TodoList() {
                 }   
             ]);
 
-    const handleAdd=(todo)=>setTodos([...todos, todo]);
+            const handleAdd=(todo)=>setTodos([...todos, todo]);
+
+            const handleUpdate = (updated)=>{ 
+                 setTodos(todos.map((t)=>
+                (t.id === updated.id? updated: t))); 
+            };
+
+            const handleDelete = (deleted)=>{
+                 setTodos(todos.filter(t=>t.id !== deleted.id))
+            }
         
     return <section>
+<ul>
+    {todos.map(item =>(
+     <Todo
+  key ={item.id}
+  todo={item}
+  onUpdate={handleUpdate}
+  onDelete={handleDelete}
+     />
+     ))}
 
-
-
-<ul>{todos.map(item =><li key={item.id}>{item.text}</li>)}</ul> 
-
+</ul>
             <AddTodo onAdd={handleAdd} />
         </section>
 }
